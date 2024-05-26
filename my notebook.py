3d35e9f4fdd4738584a2544f7b9e846ce0939f59
bdour00 <120967410+bdour00@users.noreply.github.com>
2024-05-26 09:35:23 +0300

    Add files via upload

---
+++ my notebook.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Title of the Notebook
+
+# ## Introduction
+# This notebook will cover various exercises to demonstrate basic data science skills.
+# 
+
+# ## Data Science Languages
+# - Python
+# - R
+# - SQL
+# - Julia
+# - Scala
+# 
+
+# ## Data Science Libraries
+# - Pandas
+# - NumPy
+# - Matplotlib
+# - Scikit-learn
+# - TensorFlow
+# - Keras
+# - ggplot2
+# 
+
+# ## Data Science Tools
+# 
+# | Tool            | Description                       |
+# |-----------------|-----------------------------------|
+# | Jupyter Notebook| Interactive computing environment |
+# | RStudio         | Integrated development environment for R |
+# | Apache Hadoop   | Framework for distributed storage and processing of big data |
+# | Apache Spark    | Unified analytics engine for big data processing |
+# | Tableau         | Data visualization tool           |
+# 
+
+# ## Arithmetic Expression Examples
+# Here we will demonstrate basic arithmetic operations.
+# 
+
+# In[2]:
+
+
+# Multiplication and Addition
+a = 5
+b = 3
+result_multiply = a * b
+result_add = a + b
+result_multiply, result_add
+
+
+# In[3]:
+
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+hours
+
+
+# ## Objectives
+# - Understand basic Markdown syntax
+# - Learn how to create and run code cells in Jupyter Notebook
+# - Familiarize with common data science tools and libraries
+# - Perform basic arithmetic operations
+# 
+
+# # Author
+# # Budour Alshehri
+# 
+
+# In[ ]:
+
+
+
+
